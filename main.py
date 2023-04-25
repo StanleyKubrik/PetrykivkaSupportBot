6a311f6b4ef292fa30e39cff45b80038a3441803
StanleyKubrik <37335329+StanleyKubrik@users.noreply.github.com>
2023-04-25 16:15:24 +0300

    Change requirements.txt.

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import exchange as e
-from telegram_bot import bot
+from utils import exchange as e
+from bot import telegram_bot
 from sql import SQL
-from sqlalchemy import text
 
 if __name__ == '__main__':
-    bot.polling()
-    # result = e.exchangeRun('Peremoga')
-    # print(result)
-    # e.runTask('ExchangeSQL-Petrykivka', None, 'current')
+    telegram_bot.bot.polling()
 
     # print(SQL().get_people_by_telegram_id(329518676))
